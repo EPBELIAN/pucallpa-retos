05679f1f34ae18622b85a9ab20a4bed841dc16c9
Elian Pezo Bardales <elianepb9@gmail.com>
2026-06-02 21:30:32 -0500

    fix anon key legacy

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://czqdjvxghgnhhkvxuzqa.supabase.co";
 
-const supabaseKey = "eyJhbGc..."; // pega aquí la anon key
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6cWRqdnhnaGduaGhrdnh1enFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NDI0MjUsImV4cCI6MjA5NDUxODQyNX0.fmT6c2h2cVMDzyQVqEq8HVf741VnsgM6QNwc42i04tA";
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
