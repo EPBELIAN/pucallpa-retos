d69d2dab06a36d28bd06198ae5a7407a69fdf803
Elian Pezo Bardales <elianepb9@gmail.com>
2026-06-19 07:38:32 -0500

    Corregir llamada Supabase sin api key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,13 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "TU_URL";
-const supabaseAnonKey = "TU_ANON_KEY";
+const supabaseUrl = "https://czqdjvxghgnhhkvxuzqa.supabase.co";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6cWRqdnhnaGduaGhrdnh1enFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NDI0MjUsImV4cCI6MjA5NDUxODQyNX0.fmT6c2h2cVMDzyQVqEq8HVf741VnsgM6QNwc42i04tA";
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true,
-    storage: window.localStorage,
-  },
+  }
 });
